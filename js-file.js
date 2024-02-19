22329b3ba7580efc0606f7085cfa9dac9b086ba6
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-02-19 21:15:13 +0800

    Add makeGrid func for new grid
    Add function for the prompt button's event listener and add function for the new grid.

--- js-file.js
+++ js-file.js
@@ -1,8 +1,18 @@
 const container = document.querySelector(".container");
 const sidesPrompt = document.querySelector(".numPrompt");
 
-for (let i = 0; i < 256; i++) {
-  const div = document.createElement("div");
-  div.classList.add("childDiv");
-  container.appendChild(div);
+function makeGrid(number) {
+  for (let i = 0; i < number; i++) {
+    const div = document.createElement("div");
+    div.classList.add("childDiv");
+    container.appendChild(div);
+    const tileSides = 1000 / number;
+    childDiv.style.width = tileSides;
+    childDiv.style.height = tileSides;
+  }
 }
+
+sidesPrompt.addEventListener("click", function () {
+  const numSides = +prompt("How many squares per side? Max of 100");
+  makeGrid(numSides);
+});
