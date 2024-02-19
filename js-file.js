217c249ecf8c414825a7b20d0f40d78e2517e2ea
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-02-19 21:07:34 +0800

    Reference the button element via DOM

--- js-file.js
+++ js-file.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+const sidesPrompt = document.querySelector(".numPrompt");
 
 for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
