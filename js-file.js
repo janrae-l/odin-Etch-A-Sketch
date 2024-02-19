d50003459ebfcb4669d479ceb6c162e33c1ea23d
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-02-19 21:35:15 +0800

    Test the makeGrind CSS code for the new grid size

--- js-file.js
+++ js-file.js
@@ -2,13 +2,12 @@ const container = document.querySelector(".container");
 const sidesPrompt = document.querySelector(".numPrompt");
 
 function makeGrid(number) {
-  for (let i = 0; i < number; i++) {
-    const div = document.createElement("div");
-    div.classList.add("childDiv");
-    container.appendChild(div);
-    const tileSides = 1000 / number;
-    childDiv.style.width = tileSides;
-    childDiv.style.height = tileSides;
+  for (let i = 0; i < number * number; i++) {
+    const childDiv = document.createElement("div");
+    childDiv.classList.add("childDiv");
+    container.appendChild(childDiv);
+    const tileSides = Math.round(1000 / (number * number));
+    childDiv.style.width = tileSides + "px";
   }
 }
 
