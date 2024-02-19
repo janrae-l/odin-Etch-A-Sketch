4bbd82986876dba5b51bc888d6209a6a5085693e
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-02-19 16:56:04 +0800

    Edit the for loop logic to fix the problem

--- js-file.js
+++ js-file.js
@@ -1,7 +1,7 @@
 const container = document.querySelector(".container");
 
 for (let i = 0; i <= 256; i++) {
-  const div = document.createElement(div);
-  div.classList("childDiv");
+  const div = document.createElement("div");
+  div.classList.add("childDiv");
   container.appendChild(div);
 }
