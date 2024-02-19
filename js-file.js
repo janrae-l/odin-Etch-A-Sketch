60650833ef62799a5994e1845fd66dbb390c1db5
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-02-19 18:06:39 +0800

    Edit the 16x16 grid logic

--- js-file.js
+++ js-file.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
 
-for (let i = 0; i <= 256; i++) {
+for (let i = 0; i < 256; i++) {
   const div = document.createElement("div");
   div.classList.add("childDiv");
   container.appendChild(div);
