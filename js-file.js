649fb2143eeebb5c68671bebd3a9e3e191712e8a
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-02-19 16:18:35 +0800

    Made logic for thhe 16x16 div

--- js-file.js
+++ js-file.js
@@ -3,3 +3,9 @@ const body = document.querySelector("body");
 const container = document.createElement("div");
 container.classList("container");
 body.appendChild(container);
+
+for (let i = 0; i <= 256; i++) {
+  const div = document.createElement(div);
+  div.classList("childDiv");
+  container.appendChild(div);
+}
