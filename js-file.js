ec58e1898dfbfdb4cd3c6bceface8ae1e16bda4e
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-02-20 10:07:35 +0800

    Made a for loop to further understand the underlying problem of the makeGrid func

--- js-file.js
+++ js-file.js
@@ -1,13 +1,21 @@
 const container = document.querySelector(".container");
 const sidesPrompt = document.querySelector(".numPrompt");
 
+for (let i = 0; i < 256; i++) {
+  let childDiv = document.createElement("div");
+  childDiv.classList.add("childDiv");
+  container.appendChild(childDiv);
+}
+
 function makeGrid(number) {
+  const tileSides = 960 / (number * number);
   for (let i = 0; i < number * number; i++) {
-    const childDiv = document.createElement("div");
+    childDiv = document.createElement("div");
     childDiv.classList.add("childDiv");
+    childDiv.style.height = `${tileSides}px`;
+    childDiv.style.width = `${tileSides}px`;
+
     container.appendChild(childDiv);
-    const tileSides = Math.round(1000 / (number * number));
-    childDiv.style.width = tileSides + "px";
   }
 }
 
