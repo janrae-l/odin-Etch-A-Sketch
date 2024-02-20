399f4c008cff62cc661c3c5573fdecc0e55b694e
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-02-20 16:33:46 +0800

    Test on the hover color logic

--- js-file.js
+++ js-file.js
@@ -2,26 +2,27 @@ const container = document.querySelector(".container");
 const sidesPrompt = document.querySelector(".numPrompt");
 
 let childDiv = document.createElement("div");
+
 for (let i = 0; i < 256; i++) {
   childDiv = document.createElement("div");
   childDiv.classList.add("childDiv");
+  childDiv.classList.add("hover");
   container.appendChild(childDiv);
 }
-childDiv.classList.add("hover");
 const hoverColor = document.querySelector(".hover");
-
-function makeGrid(number) {
+/*function makeGrid(number) {
   const tileSides = 960 / number;
   for (let i = 0; i < number * number; i++) {
     childDiv = document.createElement("div");
     childDiv.classList.add("childDiv");
+    childDiv.classList.add("hover");
     childDiv.style.height = `${tileSides}px`;
     childDiv.style.width = `${tileSides}px`;
 
     container.appendChild(childDiv);
   }
-}
-
+}*/
+console.log(childDiv.classList.contains("hover"));
 sidesPrompt.addEventListener("click", function () {
   let numSides = 0;
   do {
@@ -31,11 +32,22 @@ sidesPrompt.addEventListener("click", function () {
   makeGrid(numSides);
 });
 
+hoverColor.forEach((tileChild) => {
+  tileChild.addEventListener("mouseenter", function () {
+    // let randomColor = Math.floor(Math.random() * 255);
+    hoverColor.style.background = "black"; //`rgb(${randomColor}, ${randomColor}, ${randomColor})`;
+  });
+
+  tileChild.addEventListener("mouseleave", function () {
+    hoverColor.style.background = "white";
+  });
+});
+/*
 hoverColor.addEventListener("mouseenter", function () {
-  let randomColor = Math.floor(Math.random() * 255);
-  hoverColor.style.background = `rgb(${randomColor}, ${randomColor}, ${randomColor})`;
+  // let randomColor = Math.floor(Math.random() * 255);
+  hoverColor.style.background = "black"; //`rgb(${randomColor}, ${randomColor}, ${randomColor})`;
 });
 
 hoverColor.addEventListener("mouseleave", function () {
   hoverColor.style.background = "white";
-});
+});*/
