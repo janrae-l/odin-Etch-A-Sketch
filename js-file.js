d12f3cf9458406ab49bdd41ed42991ad87d142c5
janrae-l <105533574+janrae-l@users.noreply.github.com>
2024-02-20 19:07:15 +0800

    Copied the hover logic into the makeGrid func

--- js-file.js
+++ js-file.js
@@ -9,8 +9,8 @@ for (let i = 0; i < 256; i++) {
   childDiv.classList.add("hover");
   container.appendChild(childDiv);
 }
-const hoverColor = document.querySelectorAll(".hover");
-/*function makeGrid(number) {
+
+function makeGrid(number) {
   const tileSides = 960 / number;
   for (let i = 0; i < number * number; i++) {
     childDiv = document.createElement("div");
@@ -21,7 +21,23 @@ const hoverColor = document.querySelectorAll(".hover");
 
     container.appendChild(childDiv);
   }
-}*/
+  let hoverColor = document.querySelectorAll(".hover");
+  hoverColor.forEach((tileChild) => {
+    tileChild.addEventListener("mouseenter", () => {
+      let rColor = Math.floor(Math.random() * 255);
+      let gColor = Math.floor(Math.random() * 255);
+      let bColor = Math.floor(Math.random() * 255);
+      console.log("IN");
+
+      tileChild.style.background = `rgb(${rColor}, ${gColor}, ${bColor})`;
+    });
+
+    tileChild.addEventListener("mouseleave", () => {
+      console.log("OUT");
+      tileChild.style.background = "white";
+    });
+  });
+}
 console.log(childDiv.classList.contains("hover"));
 sidesPrompt.addEventListener("click", function () {
   let numSides = 0;
@@ -30,20 +46,30 @@ sidesPrompt.addEventListener("click", function () {
   } while (numSides >= 100);
   container.textContent = "";
   makeGrid(numSides);
-});
+  /*const tileSides = 960 / numSides;
+  for (let i = 0; i < numSides * numSides; i++) {
+    childDiv = document.createElement("div");
+    childDiv.classList.add("childDiv");
+    childDiv.classList.add("hover");
+    childDiv.style.height = `${tileSides}px`;
+    childDiv.style.width = `${tileSides}px`;
 
+    container.appendChild(childDiv);
+  }*/
+});
+hoverColor = document.querySelectorAll(".hover");
 hoverColor.forEach((tileChild) => {
   tileChild.addEventListener("mouseenter", () => {
     let rColor = Math.floor(Math.random() * 255);
     let gColor = Math.floor(Math.random() * 255);
     let bColor = Math.floor(Math.random() * 255);
-    // let randomColor = Math.floor(Math.random() * 255);
-    console.log("Enter");
+    console.log("IN");
+
     tileChild.style.background = `rgb(${rColor}, ${gColor}, ${bColor})`;
   });
 
   tileChild.addEventListener("mouseleave", () => {
-    console.log("Out");
+    console.log("OUT");
     tileChild.style.background = "white";
   });
 });
